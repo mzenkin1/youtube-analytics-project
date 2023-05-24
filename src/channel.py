a6274c06383a5de9174b0e263dc4b0aaa45c1aec
Maksim Zenkin <m.a.zenkin@yandex.ru>
2023-05-25 00:54:01 +0300

    implemented class and method

--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from googleapiclient import channel
 from googleapiclient.discovery import build
 
 
@@ -12,9 +13,44 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
+
         self.channel_id = channel_id
-        pass
+
+        self.title = channel['items'][0]['snippet']['title']
+        self.description = channel['items'][0]['snippet']['description']
+        self.url = channel['items'][0]['snippet']['thumbnails']["default"]['url']
+        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.video_count = channel['items'][0]['statistics']['videoCount']
+        self.views_count = channel['items'][0]['statistics']['viewCount']
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        print(json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute(), indent=2, ensure_ascii=False))
+        print(
+            json.dumps(self.youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute(), indent=2,
+                       ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        """Класс-метод возвращающий объект для работы с YouTube API"""
+        api_key: str = os.getenv('YT_API_KEY')
+        object_get = build('youtube', 'v3', developerKey=api_key)
+        return object_get
+
+    @classmethod
+    def get_service(cls):
+        api_key: str = os.getenv('YT_API_KEY')
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        return youtube
+
+    def to_json(self, moscowpython):
+        list_json = {}
+        list_json['id'] = self.channel_id
+        list_json['title'] = self.title
+        list_json['description'] = self.description
+        list_json['url'] = self.url
+        list_json['subscriber_count'] = self.subscriber_count
+        list_json['video_count'] = self.video_count
+        list_json['views_count'] = self.views_count
+
+        with open('moscowpython.json', 'w', encoding='utf-8') as f:
+            json.dump(self.list_json, f, indent=2, ensure_ascii=False)
